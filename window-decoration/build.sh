6b999fc9aef47ed225f0b42942b51fbc1235a41a
Jonathan Chun <git@jonathanchun.com>
2020-04-05 06:35:07 -0400

    Fix build problems on Ubuntu. https://github.com/n4n0GH/hello/issues/41

---
+++ window-decoration/build.sh
@@ -0,0 +1,6 @@
+#rm -rf build
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+make
+sudo make install
